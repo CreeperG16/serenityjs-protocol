7be21a7f10a6102b61d6384646d26289c6924130
CreeperG16 <pappt1216@gmail.com>
2023-11-02 13:15:57 +0100

    Revert "Start work on EntityMetadata"
    This reverts commit 3d7ee06409af94e7e6688f6f6f3e2373b876f8eb.

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -3,6 +3,3 @@ export * from './PlayStatus';
 export * from './ResourceStatus';
 export * from './ChatTypes';
 export * from './CreditStatus';
-export * from './EntityDataKey';
-export * from './EntityDataFlag';
-export * from './EntityDataType';
