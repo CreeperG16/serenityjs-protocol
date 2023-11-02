3ffdb11d502d6a0d26332e9d557b0efe2d460f55
CreeperG16 <pappt1216@gmail.com>
2023-11-02 13:19:54 +0100

    Edit Animate packet

--- src/packets/Animate.ts
+++ src/packets/Animate.ts
@@ -1,11 +1,13 @@
 import { Packet, Serialize } from '@serenityjs/raknet.js';
-import { VarInt, Short, Bool, UInt8, Float, Int32, Long } from 'binarystream.js';
+import { VarInt, Short, Bool, UInt8, Float, Int32, Long, VarLong, ZigZag } from 'binarystream.js';
 import { Encapsulated } from '../Encapsulated';
 
 @Packet(0x2c, VarInt)
 class Animate extends Encapsulated {
-	@Serialize(VarInt) public actionID!: number;
-	@Serialize(Long) public runtimeEntityID!: number;
+	@Serialize(ZigZag) public actionId!: number; // enum?
+	@Serialize(VarLong) public runtimeId!: bigint;
+
+	// TODO: if action ID is 128 or 129 then set boat_rowing_time LF32
 }
 
 export { Animate };
